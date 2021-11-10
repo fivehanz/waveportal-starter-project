4a0416b342d0ea154f12682ff30a4223a01a413f
Hanz <22578473+fivehanz@users.noreply.github.com>
2021-11-10 21:54:03 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Button } from "@chakra-ui/button";
-import { Container, Heading, Text, Stack } from "@chakra-ui/layout";
+import { Container, Heading, Text, Stack, Box } from "@chakra-ui/layout";
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import abi from "./utils/WavePortal.json"
@@ -7,12 +7,55 @@ import abi from "./utils/WavePortal.json"
 export default function App({ Component }) {
 
   const [currentAccount, setCurrentAccount] = useState("");
+  const [allWaves, setAllWaves] = useState([]);
 
-  const contractAddress = "0x63cEcD881e3FBc1A89ff572398d28E911Ab223d2"
+  const contractAddress = "0x81C8adB5b526567B39f29Dad455b4BE1fC9f96aB"
   const contractABI = abi.abi;
 
+
+  /*
+  * Create a method that gets all waves from your contract
+  */
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+        
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const checkIfWalletIsConnected = async () => {
-    
     try {
       // check access to window.ethereum
       const {ethereum} = window;
@@ -31,6 +74,7 @@ export default function App({ Component }) {
         const account = accounts[0];
         console.log("Found an authorized account: ", account);
         setCurrentAccount(account);
+        getAllWaves();
       } else {
         console.log("No authorized account found.")
       }
@@ -75,7 +119,8 @@ export default function App({ Component }) {
         /*
         * Execute the actual wave from your smart contract
         */
-        const waveTxn = await wavePortalContract.wave();
+        // const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave("Wave", { gasLimit: 300000 });
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
@@ -83,7 +128,7 @@ export default function App({ Component }) {
 
         count = await wavePortalContract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
-        
+
       } else {
         console.log("Ethereum object doesn't exist!");
       }
@@ -99,17 +144,14 @@ export default function App({ Component }) {
 
   return (
     <Container maxW="container.xl" centerContent>
-      {/* <div className="dataContainer"> */}
-        <Heading>
+      <Heading>
         Hey there! 
-        </Heading>
+      </Heading>
 
-        <Text fontSize="3xl">
-        I am Hanz and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
-        </Text>
+      <Text fontSize="3xl">
+        I am Hanz and I worked on self-deprecating bottles, so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+      </Text>
 
-        
-      {/* </div> */}
       <Stack spacing={4} direction="row" align="center">
         <Button colorScheme="teal" variant="outline" onClick={wave}>
           Wave at Me
@@ -121,6 +163,25 @@ export default function App({ Component }) {
         </Button>
         )}
       </Stack>
+
+
+      {allWaves.map((wave, index) => {
+        return (
+          <Box w="100%" p={4} color="white" bg="teal" borderWidth="1px" borderRadius="lg" overflow="hidden"  key={index} style={{ marginTop: "16px", padding: "8px" }}>
+            <Text fontSize="xl">
+              {wave.message}
+            </Text>
+            <Text fontSize="xs">
+              From: {wave.address}
+            </Text>
+            <Text fontSize="xs">
+              When: {wave.timestamp.toString()}
+            </Text>
+          </Box>
+          )
+      })}
+
+
     </Container>
   );
 }
